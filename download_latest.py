16884ad47adc87cf11f89c3f189badc9abdf92dc
Subvius <corax847@gmail.com>
2023-02-23 22:50:16 +0300

    vcs

--- download_latest.py
+++
@@ -1,3 +0,0 @@
-from git import Repo
-
-Repo.clone_from("https://github.com/Subvius/minecraft-2d-server", "./")
---
+++ on_commit.py
@@ -0,0 +1,15 @@
+import json
+
+import lib.functions.api as api
+from lib.storage.constants import Constants
+
+consts = Constants()
+res = api.post_data(consts.api_url + "game/?update=true", data={"build_number": 1})
+
+with open("manifest.json", "r", encoding="utf-8") as f:
+    manifest: dict = json.load(f)
+
+manifest.update({"build_number": manifest.get("build_number", 0) + 1})
+
+with open("manifest.json", "w") as f:
+    json.dump(manifest, f)
