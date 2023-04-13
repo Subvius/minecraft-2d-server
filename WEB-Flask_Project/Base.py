fbb45ea055b9b9f3ac5b2cb8b83938388a8a6993
Artur <khuzhin06@mail.ru>
2023-04-13 03:26:02 +0300

    test3

--- WEB-Flask_Project/Base.py
+++ WEB-Flask_Project/Base.py
@@ -1,29 +1,16 @@
+from flask import Flask, render_template, redirect, request
 import os
 import requests
 import random
-from flask import Flask, render_template, redirect
-from data import db_session
-from data.users import User
-from data.news import News
-from forms.user import RegisterForm
-from forms.loginform import LoginForm
-from flask_login import LoginManager, login_user, current_user
+from login import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
-
-@app.route('/statistics')
-def statistics():
-    response = requests.get(f"http://minecraft2d.pythonanywhere.com/player/?player=Subvius")
+@app.route('/statistics/<nickname>')
+def statistics(nickname):
+    response = requests.get(f"http://minecraft2d.pythonanywhere.com/player/?player={nickname}")
     json1 = response.json()
     photo = random.choice(get_all_image_name(path='static/background'))
     if json1["success"]:
@@ -38,64 +25,37 @@ def get_all_image_name(path='static/cloak'):
     return images
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/shop/<nickname>', methods=['GET', 'POST'])
+def shop(nickname):
+    images = get_all_image_name()
+    if request.method == 'GET':
+        for elem in images:
+            if request.values.get(elem) == elem:
+                print(elem)
+        else:
+            return render_template('shop.html', images=images, len=len(images), name=nickname)
+    elif request.method == 'POST':
+        return render_template('shop.html', images=images, len=len(images), name=nickname)
+    return render_template('shop.html', images=images, len=len(images), name=nickname)
+
+
+@app.route('/registration', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
+        name = form.username.data
+        password = form.password.data
+        response = requests.get(f"https://minecraft2d.pythonanywhere.com/auth/?nickname={name}&password={password}")
+        json1 = response.json()
+        if not json1["success"]:
+            if json1["E"] != "incorrect password":
+                requests.get(
+                    f"https://minecraft2d.pythonanywhere.com/auth/?nickname={name}&password={password}&create=True")
+        else:
+            return redirect(f"/statistics/{name}")
 
-
-@app.route("/")
-def index():
-    db_sess = db_session.create_session()
-    # news = db_sess.query(News).filter(News.is_private != True)
-    if current_user.is_authenticated:
-        news = db_sess.query(News).filter(
-            (News.user == current_user) | (News.is_private != True))
-    else:
-        news = db_sess.query(News).filter(News.is_private != True)
-    return render_template("index.html", news=news)
-
-
-@app.route('/shop')
-def shop():
-    images = get_all_image_name()
-    return render_template('shop.html', images=images, len=len(images))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Эта почта занята")
-        user = User(
-            name=form.name.data,
-            email=form.email.data,
-            about=form.about.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
+    return render_template('registration.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/blogs.db")
-    app.run(port=8080, host='localhost')
+    app.run(host='localhost', port=8080)
